f43253445250d62408c09da8f7c0701c858660ce
Trong <princ3od@gmail.com>
2021-10-07 22:56:53 +0700

    Add launcher icon and native splash screen - add assets folder - add asset_strings for asset paths management - upgrade kotlinVersion to 1.4.32 - add multidex configruation

---
+++ lib/app/core/values/asset_strings.dart
@@ -0,0 +1,8 @@
+class AssetStrings {
+  static const String _baseImageFolder = 'assets\\images';
+
+  static const String logo = '$_baseImageFolder\\keepital_logo.png';
+  static const String logoInSvg = '$_baseImageFolder\\keepital_logo.svg';
+  static const String logoWithBg = '$_baseImageFolder\\app_logo.png';
+  static const String logoWithBgInSvg = '$_baseImageFolder\\app_logo.svg';
+}
