881cff4b95f4b5cdf92231f472123587e24c973e
Trong <princ3od@gmail.com>
2021-10-13 20:37:20 +0700

    Add images - add facebook_logo, google_logo - add app_value

--- lib/app/core/values/asset_strings.dart
+++ lib/app/core/values/asset_strings.dart
@@ -1,8 +1,18 @@
-class AssetStrings {
+class AssetStringsPng {
   static const String _baseImageFolder = 'assets\\images';
+  static const String _baseIconFolder = 'assets\\icons';
 
-  static const String logo = '$_baseImageFolder\\keepital_logo.png';
-  static const String logoInSvg = '$_baseImageFolder\\keepital_logo.svg';
+  static const String logo = '$_baseImageFolder\\keepital_logo.svg';
+  static const String logoWithBg = '$_baseImageFolder\\app_logo.svg';
+  static const String financialGoal = '$_baseImageFolder\\financial_goal.svg';
+}
+
+class AssetStringsSvg {
+  static const String _baseImageFolder = 'assets\\images';
+  static const String _baseIconFolder = 'assets\\icons';
+
   static const String logoWithBg = '$_baseImageFolder\\app_logo.png';
-  static const String logoWithBgInSvg = '$_baseImageFolder\\app_logo.svg';
+  static const String logo = '$_baseImageFolder\\keepital_logo.png';
+  static const String gooogleLogo = '$_baseImageFolder\\google_logo.png';
+  static const String facebookLogo = '$_baseImageFolder\\facebook_logo.png';
 }
