5d65c47251a3380a602953d091a94154048e81d3
Trong <princ3od@gmail.com>
2021-11-12 00:59:04 +0700

    [ProfileScreen]: update UI - remove test.txt

--- lib/app/modules/profile/widgets/profile_tile.dart
+++ lib/app/modules/profile/widgets/profile_tile.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:keepital/app/core/values/app_colors.dart';
 
 class ProfileTile extends StatelessWidget {
-  final String? title;
+  final String title;
   final Function()? action;
   final IconData? iconData;
+  final Color? textColor;
+  final bool isShownTrailingIcon;
 
-  ProfileTile({this.title, this.action, this.iconData});
+  ProfileTile({required this.title, this.action, this.iconData, this.textColor, this.isShownTrailingIcon = true});
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: action,
-      child: Container(
-          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-          height: 34,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  (iconData == null) ? Icon(Icons.ac_unit_outlined) : Icon(iconData),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  SizedBox(height: 10, child: Text(title ?? "", style: Theme.of(context).textTheme.subtitle1)),
-                ],
-              ),
-              Icon(Icons.keyboard_arrow_right),
-            ],
-          )),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        child: ListTile(
+          leading: iconData != null
+              ? Icon(
+                  iconData,
+                  size: 22,
+                  color: textColor ?? AppColors.textColor.withOpacity(0.5),
+                )
+              : null,
+          trailing: (isShownTrailingIcon)
+              ? Icon(
+                  Icons.arrow_forward_ios,
+                  size: 12,
+                  color: textColor ?? AppColors.textColor,
+                )
+              : null,
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.bodyText1!.apply(color: textColor ?? AppColors.textColor),
+          ),
+        ),
+      ),
     );
   }
 }
