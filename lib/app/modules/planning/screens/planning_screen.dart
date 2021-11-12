f36e57e29244d8907f01812ad451993cd397e6e4
Trong <princ3od@gmail.com>
2021-11-13 01:38:23 +0700

    [Planning]: update UI

--- lib/app/modules/planning/screens/planning_screen.dart
+++ lib/app/modules/planning/screens/planning_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
 import 'package:keepital/app/core/theme/app_theme.dart';
-import 'package:keepital/app/modules/planning/widgets/planning_panel.dart';
 import 'package:keepital/app/modules/planning/widgets/planning_tile.dart';
 import 'package:keepital/app/routes/pages.dart';
 
@@ -13,59 +13,40 @@ class PlanningScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.currentTheme.backgroundColor,
-      body: SafeArea(
-        child: Container(
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                PlanningPanel(
-                    txtTitle: "Planning".tr,
-                    action: () => {},
-                    icon: Icon(Icons.arrow_drop_down)),
-                PlanningTile(
-                  iconData: Icons.work,
-                  title: "Budgets".tr,
-                  subtitle:
-                      "A financial plan to balance your income and expense".tr,
-                  action: () => {},
-                ),
-                PlanningTile(
-                  iconData: Icons.event,
-                  title: "Events".tr,
-                  subtitle:
-                      "Tracking on your spending during an actual event".tr,
-                  action: () {
-                    Get.toNamed(Routes.event);
-                  },
-                ),
-                PlanningTile(
-                  iconData: Icons.autorenew,
-                  title: "Recurring Transactions".tr,
-                  subtitle:
-                      "Transactions that are automatically added in future".tr,
-                  action: () => {},
-                ),
-                Spacer(),
-                PlanningTile(
-                  iconData: Icons.autorenew,
-                  title: "Recurring Transactions".tr,
-                  subtitle: "Transactions that are automatically added in future".tr,
-                  action: () => {},
-                ),
-                Spacer(),
-                PlanningTile(
-                  iconData: Icons.receipt,
-                  title: "Recurring Payments".tr,
-                  subtitle: "Monitoring your repetitive bills".tr,
-                  action: () => {},
-                ),
-                Spacer(flex: 10),
-              ],
+      appBar: AppBar(
+        title: Text('Planning'.tr),
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PlanningTile(
+              iconData: FontAwesomeIcons.suitcase,
+              title: "Budgets".tr,
+              subtitle: "A financial plan to balance your income and expense".tr,
+              action: () => {},
             ),
-          ),
+            PlanningTile(
+              iconData: Icons.event,
+              title: "Events".tr,
+              subtitle: "Tracking on your spending during an actual event".tr,
+              action: () {
+                Get.toNamed(Routes.event);
+              },
+            ),
+            PlanningTile(
+              iconData: Icons.autorenew,
+              title: "Recurring Transactions".tr,
+              subtitle: "Transactions that are automatically added in future".tr,
+              action: () => {},
+            ),
+            PlanningTile(
+              iconData: Icons.receipt,
+              title: "Recurring Payments".tr,
+              subtitle: "Monitoring your repetitive bills".tr,
+              action: () => {},
+            ),
+          ],
         ),
       ),
     );
