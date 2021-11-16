b6fa56641946deece65592cee2007d67be0a90b4
Trong <princ3od@gmail.com>
2021-11-16 07:40:24 +0700

    [EventScreen]: update UI - add textfield_with_icon_picker_item - update clickable_list_item - add events_controller

---
+++ lib/app/global_widgets/textfield_with_icon_picker_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:keepital/app/core/utils/image_view.dart';
+import 'package:keepital/app/core/values/app_colors.dart';
+import 'package:keepital/app/core/values/assets.gen.dart';
+
+class TextfieldWithIconPicker extends StatelessWidget {
+  TextfieldWithIconPicker({
+    Key? key,
+    required this.textEditingController,
+    this.hintText = '',
+    required this.onPressed,
+    this.textSize = 16,
+    this.iconId,
+    this.iconSize,
+  }) : super(key: key);
+
+  final TextEditingController textEditingController;
+  final String hintText;
+  final String? iconId;
+  final Function() onPressed;
+  final double? textSize;
+  final double? iconSize;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 8),
+        Expanded(
+            flex: 1,
+            child: InkWell(
+              onTap: onPressed,
+              child: Column(
+                children: [
+                  SizedBox(height: 13),
+                  ImageView(
+                    iconId ?? Assets.iconsUnknown.path,
+                    size: iconSize ?? 36,
+                  ),
+                  Divider(
+                    thickness: 2,
+                  ),
+                ],
+              ),
+            )),
+        SizedBox(width: 8),
+        Expanded(
+          flex: 7,
+          child: TextField(
+            controller: textEditingController,
+            style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: textSize),
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(color: AppColors.textColor.withOpacity(AppColors.disabledTextOpacity - 0.1), fontSize: textSize),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/app/modules/event/screens/add_event_screen.dart
+++ lib/app/modules/event/screens/add_event_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 import 'package:keepital/app/core/values/app_colors.dart';
-import 'package:keepital/app/core/values/asset_strings.dart';
 import 'package:keepital/app/global_widgets/clickable_list_item.dart';
 import 'package:keepital/app/global_widgets/section_panel.dart';
-import 'package:keepital/app/modules/event/widgets/text_feild_with_lead_icon.dart';
+import 'package:keepital/app/global_widgets/textfield_with_icon_picker_item.dart';
+import 'package:keepital/app/routes/pages.dart';
 
 class AddEventScreen extends StatefulWidget {
   @override
@@ -16,12 +16,8 @@ class AddEventScreen extends StatefulWidget {
 }
 
 class _AddEventScreen extends State<AddEventScreen> {
-  late DateTime pickedDate = new DateTime(1900);
-
   @override
   Widget build(BuildContext context) {
-    var formatter = new DateFormat('yyyy-MM-dd');
-    var dateNow = formatter.format(DateTime.now());
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
@@ -54,44 +50,31 @@ class _AddEventScreen extends State<AddEventScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  TextfieldWithIconPicker(
+                    onPressed: () async {
+                      final result = await Get.toNamed(Routes.selectIcon);
+                    },
+                    hintText: 'event_name'.tr,
+                    textEditingController: TextEditingController(),
+                  ),
                   ClickableListItem(
+                    hintText: 'Ending date'.tr,
+                    leading: Icon(Icons.calendar_today),
                     onPressed: () {},
+                  ),
+                  ClickableListItem(
+                    hintText: 'Wallet name'.tr,
+                    leading: Icon(FontAwesomeIcons.wallet),
+                    onPressed: () {},
+                  ),
+                  ClickableListItem(
+                    hintText: 'currency'.tr,
                     leading: Icon(
-                      Icons.calendar_today,
-                      color: Colors.black,
+                      FontAwesomeIcons.dollarSign,
                     ),
-                  ),
-                  TextFieldWithLeadIcon(imagePath: AssetStringsPng.unknownCategory, hint: 'event_name'.tr),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 10),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10, right: 10),
-                          child: Container(
-                            child: Image.asset(AssetStringsPng.calendar),
-                            width: 30,
-                            height: 30,
-                          ),
-                        ),
-                        InkWell(
-                          child: Text(
-                            pickedDate == DateTime(1900) ? dateNow : formatter.format(pickedDate),
-                            style: TextStyle(decoration: TextDecoration.underline),
-                          ),
-                          onTap: () {
-                            FocusScope.of(context).requestFocus(new FocusNode());
-                            showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(1900), lastDate: DateTime(2100)).then((date) {
-                              setState(() {
-                                pickedDate = date!;
-                              });
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  TextFieldWithLeadIcon(imagePath: AssetStringsPng.currency, hint: 'currency'.tr),
+                    text: '',
+                    onPressed: () {},
+                  )
                 ],
               ),
             ),
