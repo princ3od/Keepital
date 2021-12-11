618f8dd4f4f0e2315c2666badb7e0dc6c5ea5f9f
Huấn Nguyễn <66458858+Yud-Bet@users.noreply.github.com>
2021-12-11 18:45:44 +0700

    Update budget spent on wallet change & grouping budget (#41)
    * [feat] add peoples to transaction
    
    * Add convert money in transaction screen
    
    * Refactoring stuffs
    
    AddTransactionScreen can now change category icon depends on chose category
    
    * Remove unused imports
    
    * Add UI for Recurring Transaction screen & Refactor
    
    Refactor wallet selecting (move wallet data to data service)
    Add WalletButton
    Add RecurringTransaction model
    Add RepetitionOptions model
    Add RecurringTransactionProvider
    
    * feat AddRecurringTransaction
    
    * feat AddRecurringTransaction
    
    * [feat] edit & delete recurring transaction
    
    * Fix home appbar not update when add new trans
    
    * [feat] Auto add recurring transactions
    
    Add compare method for date (DateTime type)
    Add auto execute feature to recurring transaction
    
    * [feat] Budget UI
    
    Add translation stuffs
    
    * [feat[ Add budget
    
    * Add DetailViewAppBar
    
    * [feat] delete budget
    
    * [feat] Modify budget
    
    * Auto close budget
    
    App can now save current wallet state
    
    * Refactoring stuffs
    
    * [feat] update budget & grouping budget
    
    Update budget spent when transaction has changes
    Grouping budget into card

--- lib/app/modules/budget/widgets/finished_budget_tab.dart
+++ lib/app/modules/budget/widgets/finished_budget_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:keepital/app/core/utils/utils.dart';
 import 'package:keepital/app/data/models/budget.dart';
-import 'package:keepital/app/modules/budget/widgets/budget_item.dart';
+import 'package:keepital/app/modules/budget/widgets/budget_container.dart';
 
 class FinishedBudgetTab extends StatelessWidget {
   const FinishedBudgetTab({Key? key, required this.budgets}) : super(key: key);
@@ -8,11 +9,23 @@ class FinishedBudgetTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, List<Budget>> sortedBudgets = {};
+    budgets.forEach((element) {
+      String key = '${element.beginDate.numbericDate} - ${element.endDate.numbericDate}';
+      if (sortedBudgets.containsKey(key)) {
+        sortedBudgets[key]!.add(element);
+      } else
+        sortedBudgets[key] = [element];
+    });
+
     return ListView.builder(
       physics: BouncingScrollPhysics(),
-      itemCount: budgets.length,
       padding: const EdgeInsets.all(8),
-      itemBuilder: (context, index) => BudgetItem(budget: budgets[index]),
+      itemCount: sortedBudgets.length,
+      itemBuilder: (context, index) {
+        var key = sortedBudgets.keys.elementAt(index);
+        return BudgetContainer(budgets: sortedBudgets[key]!);
+      },
     );
   }
 }
